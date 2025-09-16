cdbf0db01504042aabb3d700a92f8bbae62a9705
Punsiri Boonyakiat <punsiri.boo@gmail.com>
2025-09-16 16:35:05 +0000

    add a2a and REadME

--- day_2/3_ADK/4_a2a/remote_agent/news_analyst/agent.py
+++ day_2/3_ADK/4_a2a/remote_agent/news_analyst/agent.py
@@ -2,6 +2,7 @@ from google.adk.agents import Agent
 from google.adk.tools import google_search
 from datetime import datetime
 import pytz
+from google.adk.a2a.utils.agent_to_a2a import to_a2a
 
 def get_current_time():
     """คืนค่าเวลาปัจจุบันในโซนเวลา Bangkok (Asia/Bangkok)
@@ -26,3 +27,26 @@ news_analyst = Agent(
     tools=[google_search, get_current_time],
 )
 
+
+skill = AgentSkill(
+    id='get_exchange_rate',
+    name='Currency Exchange Rates Tool',
+    description='Helps with exchange values between various currencies',
+    tags=['currency conversion', 'currency exchange'],
+    examples=['What is exchange rate between USD and GBP?'],
+)
+Then as part of the AgentCard it will list the agent's skills and capabilities alongside additional details like input and output modes that the agent can handle:
+
+
+# A2A Agent Card definition
+agent_card = AgentCard(
+    name='Currency Agent',
+    description='Helps with exchange rates for currencies',
+    url=f'http://{host}:{port}/',
+    version='1.0.0',
+    defaultInputModes=["text"],
+    defaultOutputModes=["text"],
+    capabilities=AgentCapabilities(streaming=True),
+    skills=[skill],
+)
+a2a_app = to_a2a(news_analyst, port=8001)
