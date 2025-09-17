07a85dc735d2bf6db6bf6e709758c08dbba85857
Witthawin Sripheanpol <witthawin@edvisory.co.th>
2025-09-17 19:27:56 +0700

    Add requirements to workshop Day1 (all) and Day2_1 and delete same not use feature

--- day_1/1_LINE_with_LLM/app/services/langgraph.py
+++ day_1/1_LINE_with_LLM/app/services/langgraph.py
@@ -7,8 +7,6 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langchain.chat_models import init_chat_model
 from langchain.schema import HumanMessage, AIMessage, SystemMessage, BaseMessage
-from langchain_core.messages import ToolMessage
-from langchain_core.tools import BaseTool
 
 from app.config import settings
 
