449493eba98c02119e7db58961fd50f6d3c3aefe
Punsiri Boonyakiat <punsiri.boo@gmail.com>
2025-09-21 03:27:01 +0000

    change ai agent and add lib python

--- day_2/3_ADK/4_a2a/agent.py
+++ day_2/3_ADK/4_a2a/agent.py
@@ -4,7 +4,7 @@ from google.adk.agents.remote_a2a_agent import RemoteA2aAgent
 root_agent = RemoteA2aAgent(
     name="news_analyst",
     description=(
-        "Helpful new assistant help user analyze recent new."
+        "คุณคือผู้ช่วยการท่องเที่ยวที่เชี่ยวชาญด้านการวางแผนการเดินทางและที่พัก Airbnb"
     ),
     agent_card=f"http://localhost:8001/{AGENT_CARD_WELL_KNOWN_PATH}",
 )
