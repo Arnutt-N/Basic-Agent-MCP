46ade2fd31ecf45c48069d52b8d2ac685f4371fd
Punsiri Boonyakiat <punsiri.boo@gmail.com>
2025-09-21 04:17:59 +0000

    rename Agent Name

--- day_2/3_ADK/4_a2a/agent.py
+++ day_2/3_ADK/4_a2a/agent.py
@@ -2,7 +2,7 @@ from google.adk.agents.remote_a2a_agent import AGENT_CARD_WELL_KNOWN_PATH
 from google.adk.agents.remote_a2a_agent import RemoteA2aAgent
 
 root_agent = RemoteA2aAgent(
-    name="news_analyst",
+    name="travel_agent",
     description=(
         "คุณคือผู้ช่วยการท่องเที่ยวที่เชี่ยวชาญด้านการวางแผนการเดินทางและที่พัก Airbnb"
     ),
